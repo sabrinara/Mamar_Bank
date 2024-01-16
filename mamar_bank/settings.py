df30d8db2e15f6e0098e30bf73c4b1f68ff53e87
sabrinara <you@example.com>
2024-01-16 18:36:28 +0600

    some changes done

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import dj_database_url
 from pathlib import Path
+# import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env("DB_NAME"),
-#         'USER': env("DB_USER"),
-#         'PASSWORD': env("DB_PASSWORD"),
-#         'HOST': env("DB_HOST"),
-#         'PORT': env("DB_PORT"),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://mamar_bank_yqoe_user:KSoYqhUUYK2HuCTVtk4zPJEx1mDqHhgA@dpg-cmilhven7f5s73a4snb0-a.oregon-postgres.render.com/mamar_bank_yqoe',
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Feel free to alter this value to suit your needs.
+#         default='postgres://mamar_bank_yqoe_user:KSoYqhUUYK2HuCTVtk4zPJEx1mDqHhgA@dpg-cmilhven7f5s73a4snb0-a.oregon-postgres.render.com/mamar_bank_yqoe',
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
